c0dea9f8d6db27b17b299a07edff5d852a59f1af
Dan <ddacunha012001@gmail.com>
2025-02-18 00:34:37 -0500

    feat: proyect init

---
+++ WPF_AG_CSG/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_AG_CSG
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
